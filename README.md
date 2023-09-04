4da8a45fd0b094ed2fe0b1abd226ec84e960efda
lenguyenkms <nguyenl@kms-technology.com>
2023-09-04 21:18:19 +0700

    First Blood

--- README.md
+++ README.md
@@ -1,24 +1,27 @@
-# Babylon 101
+# babylonjs-typescript-starter
 
-## Project setup
-```
-npm install
-```
+This is a minimalist starter kit for Babylon.js using Vite instead of webpack.
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+To start the dev server, run: `yarn dev`.
 
-### Compiles and minifies for production
-```
-npm run build
-```
+The sample scene from this guide is included: https://doc.babylonjs.com/guidedLearning/createAGame/gettingSetUp
 
-### Lints and fixes files
-```
-npm run lint
-```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Commands
+
+### `yarn dev`
+
+Start the local development server with HMR.
+
+### `yarn build`
+
+Generate a production build to directory specified by `--outDir`.
+
+### `yarn preview`
+
+Start a server to preview the static files of the production build.
+
+### `yarn lint`
+
+Lint files. Specify `--fix` to perform an inplace fix.
+# babylonjs-fish-playground
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,57 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "./style.css"
+import "@babylonjs/core/Debug/debugLayer"
+import "@babylonjs/inspector"
+import {
+    Engine,
+    Scene,
+    ArcRotateCamera,
+    Vector3,
+    HemisphericLight,
+    MeshBuilder,
+} from "@babylonjs/core"
 
-createApp(App).mount('#app')
+class App {
+    constructor() {
+        let canvas = document.createElement("canvas")
+        canvas.style.width = "100%"
+        canvas.style.height = "100%"
+        canvas.id = "gameCanvas"
+        document.getElementById("app")!.appendChild(canvas)
+
+        // initialize babylon scene and engine
+        let engine = new Engine(canvas, true)
+        let scene = new Scene(engine)
+
+        let camera = new ArcRotateCamera(
+            "Camera",
+            Math.PI / 2,
+            Math.PI / 2,
+            2,
+            Vector3.Zero(),
+            scene
+        )
+        camera.attachControl(canvas, true)
+
+        new HemisphericLight("light1", new Vector3(1, 1, 1), scene)
+        MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene)
+
+        scene.debugLayer.show()
+        
+        // hide/show the Inspector
+        window.addEventListener("keydown", ev => {
+            if (ev.key === "i") {
+                if (scene.debugLayer.isVisible()) {
+                    scene.debugLayer.hide()
+                } else {
+                    scene.debugLayer.show()
+                }
+            }
+        })
+
+        engine.runRenderLoop(() => {
+            scene.render()
+        })
+    }
+}
+
+new App()
